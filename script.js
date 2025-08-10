e551b6eb394f5e7c30f70db9df79d75d0ec790e0
TonySoprano2000 <b.mitrusic2000@gmail.com>
2025-08-10 21:06:07 +0200

    Prvi commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const { v4: uuidv4 } = require('uuid');
+
+app.use(express.urlencoded({ extended: true }));
+const path = require('path');
+
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+const comments = [
+    {id:uuidv4(), username: 'Jane', comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' },
+    {id:uuidv4(), username: 'John', comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' },
+    {id:uuidv4(), username: 'Aca',  comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' },
+    {id:uuidv4(), username: 'Bojan', comment: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' }
+];
+
+
+
+app.get('/comments',(req,res)=>{
+    res.render('comments/index',{comments});
+})
+
+app.get('/comments/new',(req,res)=>{
+    res.render('comments/new');
+})
+
+app.post('/comments',(req,res)=>{
+    const {username, comment} = req.body;
+    comments.push({username,comment,id:uuidv4()});
+    res.redirect('/comments');
+})
+
+app.get('/comments/:id',(req,res)=>{
+    const {id} = req.params;
+    const comment = comments.find((c)=>c.id === id);
+    res.render('comments/show',{comment});
+})
+
+
+
+app.listen(3030,()=>{
+    console.log("Server running on port 3030");
+})
+
+
